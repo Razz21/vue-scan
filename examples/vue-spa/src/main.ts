bda744c045a3e2ba355aecc6a711709a2d4ba225
Andrzej Kurek <32792205+Razz21@users.noreply.github.com>
2025-03-23 12:10:33 +0100

    fix: downgrade tailwind to v3 to enable stackblitz preview (#9)

--- examples/vue-spa/src/main.ts
+++ examples/vue-spa/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import './style.css';
-import { VueScanPlugin } from 'vue-scan';
+import { VueScanPlugin } from '@razz21/vue-scan';
 import App from './App.vue';
 
 const app = createApp(App);
